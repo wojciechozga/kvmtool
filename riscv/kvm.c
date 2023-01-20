f47cf1ce3ae9cac127274ee192ccbebf85db047d
Atish Patra <atishp@rivosinc.com>
2023-01-19 16:47:21 -0800

    riscv: Define riscv specific vm_type function
    RISC-V supports CoVE VMs now. It needs to setup correct VM type if the
    user requests it.
    
    Signed-off-by: Atish Patra <atishp@rivosinc.com>

--- riscv/kvm.c
+++ riscv/kvm.c
@@ -39,6 +39,18 @@ u64 kvm__arch_default_ram_address(void)
 	return RISCV_RAM;
 }
 
+int kvm__get_vm_type(struct kvm *kvm)
+{
+	if (kvm->cfg.arch.cove_vm) {
+		if (__riscv_xlen == 64)
+			return KVM_VM_TYPE_RISCV_COVE;
+		else
+			die("CoVE VM is not supported in RV32\n");
+	} else {
+		return KVM_VM_TYPE;
+	}
+}
+
 void kvm__arch_validate_cfg(struct kvm *kvm)
 {
 }
